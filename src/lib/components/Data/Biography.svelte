0e6fbfe069c6cede0c4a6298b955720f9b11841a
gautiersylvain <laurenazeitz@gmail.com>
2025-04-17 18:10:49 -0500

    Update Biography.svelte

--- src/lib/components/Data/Biography.svelte
+++ src/lib/components/Data/Biography.svelte
@@ -6,13 +6,19 @@
 </script>
 
 <h3>Early Life</h3>
-<p>Rogue Trader Pandora Von Valancius was born on the planet of Meridian, a hive world in the Korianis System. She was transported to Holy Terra following the manifestation of her psyker abilites. The Lord Captain reports no surviving family members.</p>
+<p>Rogue Trader Pandora Von Valancius was born to humble laborers on the planet Meridian, a hive world in the Korianis System. She was transported to Holy Terra following the manifestation of her psyker abilites at a young age. The Lord Captain reports no surviving immediate family members.</p>
 
 <h3>Sanctioning</h3>
 <Classified></Classified>
 
 <h3>Astra Militarum</h3>
-<p>After completing her training under the Scholastica Psykana, Pandora entered the ranks of the Astra Militarum to begin training as a Primaris Psyker. She served in the Imperial Navy for just over 7 years and was stationed primarily in the Calixis Sector. It was during this time that the Lord Captain first came into the public eye when she single-handedly saved a pilgrim ship by stopping a warp breach.</p>
+<p>After completing her training under the Scholastica Psykana, Pandora entered the ranks of the Astra Militarum to begin training as a Primaris Psyker. She served in the Imperial Navy for just over 7 years and was stationed primarily in the Calixis Sector. It was during this time that the Lord Captain first came into the public eye when she single-handedly saved a pilgrim ship by stopping a warp breach. Pandora was honorably discharged following the incident.</p>
+
+<h3>Later Life</h3>
+<Classified></Classified>
+
+<h3>Arrival in the Koronus Expanse</h3>
+<p>About 20 years after her discharge from the Astra Militarum, Pandora arrived in the Koronus Expanse at the behest of her predecessor Rogue Trader Theodora von Valancius Massimo af Scarus.</p>
 
 <LongDivider></LongDivider>
 <h3>What else to write in this section?</h3>
