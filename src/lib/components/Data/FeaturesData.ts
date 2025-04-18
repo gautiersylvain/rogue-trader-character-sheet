8e96e15e652669b03b0be7874e02eda51dbc9faf
gautiersylvain <laurenazeitz@gmail.com>
2025-04-18 11:44:27 -0500

    Update FeaturesData.ts

--- src/lib/components/Data/FeaturesData.ts
+++ src/lib/components/Data/FeaturesData.ts
@@ -23,17 +23,17 @@ export const ActiveAbilities: activeAbilities = {
     talentTwoOpen: true,
     exemplarOpen: true,
     archetypeTalentExemplar: [],
-    archetypeTalentOne: [],
+    archetypeTalentOne: ['Focus!', 'Still Mind', 'Personal Oversight', 'Steel Resolve', ],
     archetypeTalentTwo: [`Aim for opening`, `Death Whisper`, `Death Whisper - Second Hand`, `Dispatch`, `Dispatch - Second Weapon`, `Poised to Strike`, `Feinting Attack`]
 }
 
 export const TalentsData: Talents = {
     originTabOpen: true,
-    talentOneOpen: false,
-    talentTwoOpen: false,
+    talentOneOpen: true,
+    talentTwoOpen: true,
     exemplarOpen: true,
-    originTalents: ["Hive World", "Strength in Numbers", "Sanctioned Psyker", "Sanctioned Psyker - Biomancer", "Biomancy", "Shadow of Torment", "Illustrious Glory", "Malediction of Rykad Minoris", "Celestial Inspiration", "Legacy of the Dead", "Blood of Scipione 84-249", "Otherworldly Observer", "Punisher", "Expert Cryptographer", "Frequency of Faith", "Wisdom of the Tech-Guards", "I Will Fear No Poison", "Leader of the Engine Vandals", "Dark Revelation", "The Hammer and the Cog", "Secrets of Ancient Cybersmiths", "Aeldari Machine Taming", "Disciple of the Damned", "Blessings of the Data Angels", "Battle Mastery", "Holy Woman", "Mistress of Flame", "Patroness of Cults", "Wisdom Extractor", "Merciful Saviour", "Exalted Instruments of War", "Bestower of Freedom", "Iron Arm", "Fiend of Vheabos VI"],
-    archetypeTalentOne: [],
+    originTalents: ['Hive World', 'Strength in Numbers', 'Sanctioned Psyker', 'Sanctioned Psyker - Biomancer', 'Biomancy', 'Shadow of Torment', 'Illustrious Glory', 'Malediction of Rykad Minoris', 'Celestial Inspiration', 'Legacy of the Dead', 'Blood of Scipione 84-249', 'Otherworldly Observer', 'Punisher', 'Expert Cryptographer', 'Frequency of Faith', 'Wisdom of the Tech-Guards', 'I Will Fear No Poison', 'Leader of the Engine Vandals', 'Dark Revelation', 'The Hammer and the Cog', 'Secrets of Ancient Cybersmiths', 'Aeldari Machine Taming', 'Disciple of the Damned', 'Blessings of the Data Angels', 'Battle Mastery', 'Holy Woman', 'Mistress of Flame', 'Patroness of Cults', 'Wisdom Extractor', 'Merciful Saviour', 'Exalted Instruments of War', 'Bestower of Freedom', 'Iron Arm', 'Fiend of Vheabos VI'],
+    archetypeTalentOne: ['Voice of Command', 'Bring It Down!', 'Get Back in the Fight!'],
     archetypeTalentTwo: [],
-    archetypeTalentExemplar: [`Exemplar`,"Unbreakable Will", `Sharpshooter`, `Imminent Demise`, `Aeldari Weapon Proficiency`, `Intimidation`, `Perfection Under Fire`, `Lethality Heightens`, `Ballistic Calculation`, `Danse Macabre`, `Deadly Calculation`, `AP Increase`, `Know-It-All`, `Continuous Analysis`, `Passive Learning`, `Ambush`, `Critical Velocity`, `Combat Insight`, `Outsmart`, `Sniper Expertise`, `Base Skill: Lore [Warp]`, `Feinting Attack`, `Cataclysm`, `AP Increase`, `Offensive Pattern Prediction`]
+    archetypeTalentExemplar: [`Exemplar`, 'Unbreakable Will', `Sharpshooter`, `Imminent Demise`, `Aeldari Weapon Proficiency`, `Intimidation`, `Perfection Under Fire`, `Lethality Heightens`, `Ballistic Calculation`, `Danse Macabre`, `Deadly Calculation`, `AP Increase`, `Know-It-All`, `Continuous Analysis`, `Passive Learning`, `Ambush`, `Critical Velocity`, `Combat Insight`, `Outsmart`, `Sniper Expertise`, `Base Skill: Lore [Warp]`, `Feinting Attack`, `Cataclysm`, `AP Increase`, `Offensive Pattern Prediction`]
 }
